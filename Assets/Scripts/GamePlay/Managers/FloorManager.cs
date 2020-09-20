c403906171fa2e3015f9c6ae4773d97248351083
idjaeha <jaehanim1@gmail.com>
2020-09-20 17:12:59 +0900

    Add Floor System

---
+++ Assets/Scripts/GamePlay/Managers/FloorManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HappyBread.GamePlay
+{
+    public class FloorManager : MonoBehaviour
+    {
+        public GameObject[] floors;
+        private int from, to;
+        private Vector3 exitPos;
+        public void ChangeFloor(int from, int to, Vector3 exitPos)
+        {
+            this.from = from;
+            this.to = to;
+            this.exitPos = exitPos;
+            GameModel.Instance.EffectManager.FadeOut();
+            Invoke("ChangeFloor", 2f);
+        }
+
+        private void ChangeFloor()
+        {
+            floors[from].SetActive(false);
+            floors[to].SetActive(true);
+            GameModel.Instance.Player.transform.position = exitPos;
+            GameModel.Instance.EffectManager.FadeIn(0.2f);
+        }
+    }
+}
