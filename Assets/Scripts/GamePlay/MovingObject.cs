58106dd27ad36e84a66bdb3217138854ecf4f0ad
idjaeha <jaehanim1@gmail.com>
2020-09-20 10:24:17 +0900

    Seperate Player and MovingObject

---
+++ Assets/Scripts/GamePlay/MovingObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HappyBread.GamePlay
+{
+    public class MovingObject : MonoBehaviour
+    {
+        public float speed;
+        public float speedArriveTime;
+
+        protected Vector2 objectDirection;
+        protected SpriteRenderer spriteRenderer;
+        protected Rigidbody2D rigidBody2D;
+        protected Vector2 currentVelocity;
+        protected Animator animator;
+
+        protected virtual void Move(Vector2 direction)
+        {
+            objectDirection = direction.normalized;
+            spriteRenderer.flipX = objectDirection.x > 0 ? false : objectDirection.x < 0 ? true : spriteRenderer.flipX;
+            rigidBody2D.velocity = Vector2.SmoothDamp(rigidBody2D.velocity, objectDirection * speed, ref currentVelocity, speedArriveTime);
+
+            animator.SetFloat("WalkX", objectDirection.x);
+            animator.SetFloat("WalkY", objectDirection.y);
+        }
+
+        protected virtual void Awake()
+        {
+            rigidBody2D = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+}
--- Assets/Scripts/GamePlay/Player.cs
+++ Assets/Scripts/GamePlay/Player.cs
@@ -12,7 +12,7 @@ namespace HappyBread.GamePlay
     /// <summary>
     /// 플레이어를 제어 할 수 있는 클래스.
     /// </summary>
-    public class Player : MonoBehaviour
+    public class Player : MovingObject
     {
         [HideInInspector]
         public Vector3 NextMoveCommand; // 다음 움직임 명령
@@ -21,10 +21,7 @@ namespace HappyBread.GamePlay
         public LayerMask interactableLayer;
         public LayerMask evidenceLayer;
         public float hitDistance = 0.5f;
-        public float speed = 2f;
-        public float speedArriveTime = 0.1f; // 0에 가까울 수록 빨리 해당 속도에 도달
         public float useHpAmount = 0.01f;
-
 
         private enum State
         {
@@ -33,18 +30,10 @@ namespace HappyBread.GamePlay
         }
 
         private State state;
-        private Rigidbody2D rigidBody2D;
-        private Animator animator;
-        private SpriteRenderer spriteRenderer;
-        private Vector2 playerDirection;
-        private Vector2 currentVelocity;
         private RaycastHit2D hit;
 
-        private void Awake()
+        private void Start()
         {
-            rigidBody2D = GetComponent<Rigidbody2D>();
-            animator = GetComponent<Animator>();
-            spriteRenderer = GetComponent<SpriteRenderer>();
             state = State.Idle;
         }
 
@@ -90,7 +79,7 @@ namespace HappyBread.GamePlay
         private void AttemptInteract()
         {
             Vector2 start = transform.position;
-            Vector2 end = (Vector2)transform.position + playerDirection * hitDistance;
+            Vector2 end = (Vector2)transform.position + objectDirection * hitDistance;
             hit = Physics2D.Linecast(start, end, interactableLayer);
             if (hit.transform != null)
             {
@@ -132,7 +121,7 @@ namespace HappyBread.GamePlay
         {
             if (NextMoveCommand != Vector3.zero) // 움직이라는 명령 받음
             {
-                Move();
+                Move(NextMoveCommand);
             }
             else // 움직임 상태 해제
             {
@@ -141,16 +130,11 @@ namespace HappyBread.GamePlay
             }
         }
 
-        private void Move()
+        protected override void Move(Vector2 NextMoveCommand)
         {
-            playerDirection = NextMoveCommand.normalized;
-            spriteRenderer.flipX = NextMoveCommand.x > 0 ? false : NextMoveCommand.x < 0 ? true : spriteRenderer.flipX;
-            rigidBody2D.velocity = Vector2.SmoothDamp(rigidBody2D.velocity, playerDirection * speed, ref currentVelocity, speedArriveTime);
+            base.Move(NextMoveCommand);
 
-            animator.SetFloat("WalkX", playerDirection.x);
-            animator.SetFloat("WalkY", playerDirection.y);
             NextMoveCommand = Vector3.zero;
-
             GameModel.Instance.Hp.Add(-useHpAmount); // Hp 변동
         }
 
